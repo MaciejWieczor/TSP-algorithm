d1a20b752a42b735d84d91e27f72fdcdcfd2654f
MaciejWieczor <maciejwieczor@gmail.com>
2022-12-17 19:28:27 +0100

    Add evaluation function

--- tsp.py
+++ tsp.py
@@ -16,7 +16,7 @@ class travelling_salesman_problem_algorithm:
         self.distance_matrix = networkx.to_numpy_matrix(self.graph)
 
         # get chromosome length
-        self.chromosome_length = self.distance_matrix[0].len()
+        self.chromosome_length = self.distance_matrix[0].shape[1]
 
     def initialize_random_chromosome(self):
         # randomize array of cities indexes
@@ -26,4 +26,14 @@ class travelling_salesman_problem_algorithm:
         # print the distance between nodes 4 and 2:
         return(self.distance_matrix[x, y])
 
+    def evaluate_chromosome(self, chromosome_array):
+        sum = 0
+        for i in range(0, chromosome_array.shape[0]-1):
+            sum += self.return_distance_matrix(chromosome_array[i], chromosome_array[i + 1])
+        return sum
+
+# testing
 TSP_instance = travelling_salesman_problem_algorithm()
+for i in range(0, 10): 
+    chromosome_1 = TSP_instance.initialize_random_chromosome()
+    print(f"Chromosome nr {i} score : {TSP_instance.evaluate_chromosome(chromosome_1)}")
