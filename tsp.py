137863f01c9869009acdc3e482d1b9bdd6670034
MaciejWieczor <maciejwieczor@gmail.com>
2022-12-21 20:50:12 +0100

    Skeleton setup for crossing system

--- tsp.py
+++ tsp.py
@@ -5,7 +5,7 @@ import tsplib95
 import networkx
 import sys
 
-number_of_iterations = 5000
+number_of_iterations = 1000
 
 # takes name of tsp file from the first command line argument
 class travelling_salesman_problem_algorithm:
@@ -46,7 +46,6 @@ class travelling_salesman_problem_algorithm:
         array = np.random.choice(range(1, self.chromosome_length),\
                                  self.chromosome_length-1, replace=False)
         array = np.insert(array, 0, 0)
-        print(array)
         return(array)
 
     def return_distance_matrix(self, x, y):
@@ -63,10 +62,34 @@ class travelling_salesman_problem_algorithm:
         for i in range(self.population_size):
             self.population_evaluation[i] = self.evaluate_chromosome(self.population[i])
 
-    def sort_by_evaluation(self):
-        self.population = [x for _, x in sorted(zip(self.population_evaluation,\
-                                                    self.population))]
-        self.population_evaluation.sort()
+    def crossing_function(self, i):
+        # implement OX crossover here
+        # find second parent
+        # create children
+        # evaluate children scores 
+        # if better than their parents replace the parents - return 0
+        # else - return 1
+        pass
+
+    def mutating_function(self, i):
+        pass
+
+    def update_population(self):
+        # loop through all chromosomes 
+        best_score = max(self.population_evaluation)
+        for i in range(0, self.population_size):
+            # do probability test based on current outcome against best evaluation 
+            # (best should have max probability) 
+            # here we get <0, 1> float and we multiply it by 1000
+            probability_treshold = (1000 * self.population_evaluation[i]) / best_score
+            probability_test = randint(0, 1001)
+            # if test passed then do crossing and check agains last evaluation 
+            # if better than last evaluation update member - else go to mutations
+            if(probability_test < probability_treshold):
+                do_mutation = self.crossing_function(i)
+                # mutate member if he didn't do crossing
+                if(do_mutation):
+                    self.mutating_function(i)
 
     def visualize_connections(self):
         # first translate points into two arrays
@@ -78,8 +101,6 @@ class travelling_salesman_problem_algorithm:
         # second get connections
         x_connection = []
         y_connection = []
-        print(self.node_list[self.population[0][0]])
-        print(list(self.population[0]))
         for i in range(self.chromosome_length):
             x_connection.append(self.node_list[self.population[0][i]][1][0])
             y_connection.append(self.node_list[self.population[0][i]][1][1])
@@ -87,39 +108,17 @@ class travelling_salesman_problem_algorithm:
         self.ax.plot(x_connection,y_connection)
         plt.show()
 
-    def population_crossing(self):
-        pass
-
-    def population_mutating(self):
-        pass
-
-    def get_new_population(self):
-        self.population_crossing()
-        self.population_mutating()
-
 # testing
 # create class instance
 TSP_instance = travelling_salesman_problem_algorithm(population_size=20)
 
 for k in range(number_of_iterations):
-    # evaluate first population iteration
+    # get chromosome scores
     TSP_instance.evaluate_chromosomes()
 
-    # sort population by evaluation
-    TSP_instance.sort_by_evaluation()
+    # get new population based on probability
+    TSP_instance.update_population()
 
-    # loop over each member of the population
-    # for i in range(TSP_instance.population_size): 
-    #     # print each member score
-    #     print(f"Chromosome nr {i} score : {TSP_instance.population_evaluation[i]}")
-
-        # # print each member city visit order
-        # print(TSP_instance.population[i])
-
-    print(f"Best chromosome score : {TSP_instance.population_evaluation[0]} \
-            at iteration number {k}")
-
-    # mutate best chromosome
-    TSP_instance.get_new_population()
+    print(TSP_instance.population_evaluation)
 
 TSP_instance.visualize_connections()
